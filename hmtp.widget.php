62f7a74198139e1aebd3e863b09fa3892b38f0c9
Matth_eu <matthew@matth.eu>
2014-06-30 16:13:49 +0100

    Fix widget output

--- hmtp.widget.php
+++ hmtp.widget.php
@@ -24,7 +24,7 @@ class HMTP_Widget extends WP_Widget {
 
 		// Demo.
 		$results = hmtp_get_top_posts( $instance['args'] );
-		
+
 		if ( ! $results )
 			return;
 
@@ -35,16 +35,20 @@ class HMTP_Widget extends WP_Widget {
 			echo esc_html( $instance['title'] );
 			echo $args['after_title'];
 		}
-	
-		?>		
-	
-		<ul>
+
+		?>
+
+		<ol class="hmtp-widget">
 			<?php foreach ( $results as $post ) : ?>
-				<li><?php printf( '%s (%d)', get_the_title( $post['post_id'] ), $post['views'] ); ?></li>
+				<li><?php printf(
+					'<a href="%s">%s</a>',
+					esc_url( get_permalink( $post['post_id'] ) ),
+					esc_html( get_the_title( $post['post_id'] ) )
+				); ?></li>
 			<?php endforeach; ?>
-		</ul>
-		
-		<?php 
+		</ol>
+
+		<?php
 
 		echo $args['after_widget'];
 
@@ -61,9 +65,9 @@ class HMTP_Widget extends WP_Widget {
  	public function form( $instance ) {
 
  		$title = isset( $instance[ 'title' ] ) ? $instance[ 'title' ] : 'Hive Post Authors';
- 		
- 		$args = wp_parse_args( 
- 			(array) $instance['args'], 
+
+ 		$args = wp_parse_args(
+ 			(array) $instance['args'],
  			array(
  				'count'     => 5,
  				'post_type' => array( 'post' ),
@@ -78,18 +82,18 @@ class HMTP_Widget extends WP_Widget {
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>">Title</label>
 			<input class="widefat" type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
 		</p>
-		
+
 		<p>
 			<label for="<?php echo $this->get_field_id( 'args' ); ?>-count">Count</label>
 			<input class="widefat" type="number" id="<?php echo $this->get_field_id( 'args' ); ?>-count" name="<?php echo $this->get_field_name( 'args' ); ?>[count]" value="<?php echo intval( $args['count'] ); ?>" />
-		</p>		
-		
+		</p>
+
 		<p>
 			<label for="<?php echo $this->get_field_id( 'args' ); ?>-post-type">Post Type</label>
 			<select class="widefat" multiple id="<?php echo $this->get_field_id( 'args' ); ?>-post-type" name="<?php echo $this->get_field_name( 'args' ); ?>[post_type][]">
 				<?php foreach( get_post_types( array( 'public' => true, 'publicly_queryable' => true  ) ) as $post_type ) : ?>
 					<option <?php selected( true, in_array( $post_type, (array) $args['post_type'] ) ); ?>><?php echo esc_html( $post_type ); ?></option>
-				<?php endforeach; ?> 
+				<?php endforeach; ?>
 			</select>
 		</p>
 
@@ -99,7 +103,7 @@ class HMTP_Widget extends WP_Widget {
 				<option>None</option>
 				<?php foreach ( get_taxonomies( '', 'names' ) as $tax ) : ?>
 					<option <?php selected( $args['taxonomy'], $tax ); ?>><?php echo esc_html( $tax ); ?></option>
-				<?php endforeach; ?> 
+				<?php endforeach; ?>
 			</select>
 		</p>
 
@@ -126,20 +130,20 @@ class HMTP_Widget extends WP_Widget {
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-		
+
 		$instance['args'] = array();
-		
+
 		$instance['args']['count'] = intval( $new_instance['args']['count'] );
-		
+
 		if ( ! empty( $new_instance['args']['post_type'] ) )
 			$instance['args']['post_type'] = $new_instance['args']['post_type'];
-		
-		if ( ! empty( $new_instance['args']['taxonomy'] ) && taxonomy_exists( $new_instance['args']['taxonomy'] ) ) 
+
+		if ( ! empty( $new_instance['args']['taxonomy'] ) && taxonomy_exists( $new_instance['args']['taxonomy'] ) )
 			$instance['args']['taxonomy'] = $new_instance['args']['taxonomy'];
 
 		if ( ! empty( $new_instance['args']['terms'] ) )
 			$instance['args']['terms'] = array_map( 'trim', explode( ',', $new_instance['args']['terms'] ) );
-		
+
 		return $instance;
 
 	}
